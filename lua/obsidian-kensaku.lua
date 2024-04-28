6eb1874e8a05f2d5aa74d707dc4dfb42addcc50c
delphinus <me@delphinus.dev>
2024-04-28 16:24:39 +0900

    fix: simplify to call setup in initialization

--- lua/obsidian-kensaku.lua
+++ lua/obsidian-kensaku.lua
@@ -1,18 +1,28 @@
-local done
+---@class obsidian-kensaku
+---@field setup_called boolean
+---@field setup fun(opts: obsidian-kensaku.config.SetupOpts)
+---@overload fun(client: obsidian.Client)
 
-return {
-  ---@param opts obsidian-kensaku.config.SetupOpts
-  setup = function(opts)
-    ---@param client obsidian.Client
-    return function(client)
-      if not done then
-        require("obsidian-kensaku.config").normalize(opts)
-        done = true
-      end
-      vim.api.nvim_create_user_command("ObsidianKensaku", function(data)
-        local picker = require("obsidian-kensaku.picker").new(client)
-        picker:grep_notes { query = data.args }
-      end, { nargs = "?", desc = "Search vault with kensaku.vim" })
+return setmetatable({ setup_called = false }, {
+  ---@param client obsidian.Client
+  __call = function(self, client)
+    if not self.setup_called then
+      self.setup()
     end
+    vim.api.nvim_create_user_command("ObsidianKensaku", function(data)
+      local picker = require("obsidian-kensaku.picker").new(client)
+      picker:grep_notes { query = data.args }
+    end, { nargs = "?", desc = "Search vault with kensaku.vim" })
   end,
-}
+  ---@param key string
+  __index = function(self, key)
+    if key == "setup" then
+      ---@param opts obsidian-kensaku.config.SetupOpts
+      return function(opts)
+        require("obsidian-kensaku.config").normalize(opts)
+        self.setup_called = true
+      end
+    end
+    error("Invalid key: " .. key)
+  end,
+})
